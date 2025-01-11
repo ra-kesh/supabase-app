f2b15b86772491ef14b53a8b679c010b5e59d179
ra-kesh <iamrakeshkumar@pm.me>
2025-01-11 20:35:29 +0530

    initial setup for supabase working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
+"use client";
+
+import { createSupabaseClient } from "@/supabase-utils/client";
 import Image from "next/image";
+import React, { useEffect } from "react";
 
 export default function Home() {
+  //   React.useEffect(() => {
+  //     const supabase = createSupabaseClient();
+  //     supabase.storage.listBuckets().then((result) =>
+  //      console.log("Bucket List", result)
+  //     });
+  // }, [])
+
+  useEffect(() => {
+    const supabase = createSupabaseClient();
+    supabase.storage
+      .listBuckets()
+      .then((result) => console.log("bucketlist", result));
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
