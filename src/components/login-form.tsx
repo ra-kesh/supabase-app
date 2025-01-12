a633876605eb712d95e42fd19e598c261b978727
ra-kesh <iamrakeshkumar@pm.me>
2025-01-12 19:47:47 +0530

    feat/magic link toggle with searchparams

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,19 +1,27 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
+interface LoginFormProps extends React.ComponentPropsWithoutRef<"div"> {
+  isPasswordLogin?: boolean;
+}
 
 export function LoginForm({
   className,
+  isPasswordLogin,
   ...props
-}: React.ComponentPropsWithoutRef<"div">) {
+}: LoginFormProps) {
+  console.log(isPasswordLogin);
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -35,24 +43,38 @@ export function LoginForm({
                   required
                 />
               </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
+              {isPasswordLogin && (
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="#"
+                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input id="password" type="password" required />
                 </div>
-                <Input id="password" type="password" required />
-              </div>
+              )}
               <Button type="submit" className="w-full">
                 Login
               </Button>
-              <Button variant="outline" className="w-full">
-                Login with Google
-              </Button>
+              {isPasswordLogin ? (
+                <Link href={{ pathname: "/", query: { magicLink: "yes" } }}>
+                  {" "}
+                  <Button variant="outline" className="w-full">
+                    Prefer Magiclink ?
+                  </Button>
+                </Link>
+              ) : (
+                <Link href={{ pathname: "/", query: { magicLink: "no" } }}>
+                  {" "}
+                  <Button variant="outline" className="w-full">
+                    Prefer Password ?
+                  </Button>
+                </Link>
+              )}
             </div>
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
@@ -64,5 +86,5 @@ export function LoginForm({
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
