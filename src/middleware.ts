5897fef0aa5f9d9eeb98e347674e4f7fb5a079d8
ra-kesh <iamrakeshkumar@pm.me>
2025-01-14 22:45:13 +0530

    feat/added session in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,13 @@ import { NextRequest } from "next/server";
 import { getSupabaseReqresClient } from "./supabase-utils/reqResClient";
 
 export async function middleware(request: NextRequest) {
-  const { response } = getSupabaseReqresClient({ request });
+  const { supabase, response } = getSupabaseReqresClient({ request });
+  // we are just asking supabase here to retrieve the session,by only doing it will also refresh the session if expired.
+  await supabase.auth.getSession();
+
   return response.value;
 }
+
+export const config = {
+  matcher: ["/((?!.*\\.).*)"],
+};
