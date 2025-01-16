288513f8e27526a497390b877c608af691940f3c
ra-kesh <iamrakeshkumar@pm.me>
2025-01-16 20:55:28 +0530

    feat/added logout option initial

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,7 +1,38 @@
+"use client";
+
+import { getSupabaseBrowserClient } from "@/supabase-utils/browserClient";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-const tickets = () => {
-  return <div>tickets</div>;
+const Tickets = () => {
+  const supabase = getSupabaseBrowserClient();
+  const router = useRouter();
+
+  return (
+    <div>
+      tickets
+      <Link
+        role="button"
+        href="/logout"
+        prefetch={false}
+        onClick={(event) => {
+          event.preventDefault();
+          supabase.auth.signOut().then(() => {
+            router.push("/");
+          });
+        }}
+      >
+        Log out
+      </Link>
+    </div>
+  );
 };
 
-export default tickets;
+export default Tickets;
+
+// this type of logout functionality should not be encouraged as incase of multiple logout we will have to multiple redirect
+
+// there should be an client component that is avialable on all pages that can grab the logout event no matter the page
+
+// basically it should subscribe to an event from supabase telling us that a logut has happened and you need to unscribe it
